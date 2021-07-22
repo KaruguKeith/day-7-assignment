b8c23a574be442cd3b8745bc1e5fb97b4633e887
karugu keith <87313760+KaruguKeith@users.noreply.github.com>
2021-07-22 15:27:19 +0300

    original

--- animals.py
+++ animals.py
@@ -1,4 +1,33 @@
-class Animals ():
+class AnimalBehaviour():
+    def __init__(self,name,reproduction,incubationPeriod,diet ):
+      
+        self.name = name
+        self._reproduction= reproduction
+        self._diet = diet
+        self.incubationPeriod = incubationPeriod
 
-    def __init__ animalBehaviour  (self,sound,diet,) :
+    def setAnimalData(self,name,reproduction,incubationPeriod,diet ):
+
+        self.name = name
+        name =input('Enter sound')
+        self.reproduction= reproduction
+        reproduction= input('Enter reproducttion')
+        self.diet = diet
+        diet=input('Enter diet')
+        self.incubationPeriod = incubationPeriod
+        incubationPeriod=input('Enter incubation period')
+        
+
+    def getAnimalData (self):
+       print(self.name,self.reproduction,self.diet,self.incubationPeriod)
     
+class carnivores(AnimalBehaviour):
+    pass  
+class omnivores(AnimalBehaviour):
+    pass
+class herbivores(AnimalBehaviour):
+    pass
+  
+
+newHerbivor = herbivores('Lion', 'sexually','9 months', 'leaves')
+herbivores.getAnimalData
